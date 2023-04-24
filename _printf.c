235a25a35bc81b01b22e7c7e33ab49e9542c781a
Mungw Agu <mungwaagu@gmail.com>
2023-04-24 18:05:05 +0100

    Removed int num variable

--- _printf.c
+++ _printf.c
@@ -9,7 +9,6 @@
 int _printf(const char *format, ...)
 {
 	char *str;
-	int num = 0;
 	va_list print;
 
 	va_start(print, format);
@@ -18,27 +17,23 @@ int _printf(const char *format, ...)
 		if (*format != '%')
 		{
 			_putchar(*format);
-			num += 1;
 			continue;
 		}
 		switch (*++format)
 		{
 			case 'c':
 				_putchar(va_arg(print, int));
-				num += 1;
 				break;
 			case 's':
 				str = va_arg(print, char *);
 				while (*str)
 					_putchar(*str++);
-					num += 1;
 				break;
 			default:
 				_putchar('%');
 				_putchar(*format);
-				num += 2;
 				break;
 		}
 	}
-	return (num);
+	return (0);
 }
