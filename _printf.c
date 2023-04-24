764176b52c24ef03ac4ed4830863e8b17163ba9a
Mungw Agu <mungwaagu@gmail.com>
2023-04-24 18:03:05 +0100

    Added int num variable to store the number of characters printed

--- _printf.c
+++ _printf.c
@@ -18,25 +18,25 @@ int _printf(const char *format, ...)
 		if (*format != '%')
 		{
 			_putchar(*format);
-			num+=1;
+			num += 1;
 			continue;
 		}
 		switch (*++format)
 		{
 			case 'c':
 				_putchar(va_arg(print, int));
-				num+=1;
+				num += 1;
 				break;
 			case 's':
 				str = va_arg(print, char *);
 				while (*str)
 					_putchar(*str++);
-					num+=1;
+					num += 1;
 				break;
 			default:
 				_putchar('%');
 				_putchar(*format);
-				num+=2;
+				num += 2;
 				break;
 		}
 	}
