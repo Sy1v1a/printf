90cf771f98fcc831d3d2c2e435d91ce60c50794f
sy1v1a <oyinyechi@ymail.com>
2023-04-25 15:36:20 -0700

    main

--- main.h
+++ main.h
@@ -1,7 +1,98 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#define UNUSED(x) (void)(x)
+#define BUFSIZE 1024
+#define T_MINUS 1
+#define T_PLUS 2
+#define T_ZERO 4
+#define T_HASH 8
+#define T_SPACE 16
+#define SLONG 2
+#define SSHORT 1
+
+/**
+ * struct fmt - Struct op
+ * @fmt: The format.
+ * @fn: The function associated.
+ */
+struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
+
+
+/**
+ * typedef struct fmt fmt_t - Struct op
+ * @fmt: The format
+ * @fmt: The function associated.
+ */
+typedef struct fmt fmtt;
 
 int _printf(const char *format, ...);
-int _putchar(char c);
+int print(const char *fmt, int *i,
+va_list list, char buffer[], int flag, int width, int precision, int size);
+
+
+int printchar(va_list type, char buffer[],
+	int flag, int width, int precision, int size);
+int printstring(va_list type, char buffer[],
+	int flag, int width, int precision, int size);
+int printpercent(va_list type, char buffer[],
+	int flag, int width, int precision, int size);
+int printint(va_list type, char buffer[],
+	int flag, int width, int precision, int size);
+int printbinary(va_list type, char buffer[],
+	int flag, int width, int precision, int size);
+int printunsigned(va_list type, char buffer[],
+	int flag, int width, int precision, int size);
+int printoctal(va_list type, char buffer[],
+	int flag, int width, int precision, int size);
+int printhexadecimal(va_list type, char buffer[],
+	int flag, int width, int precision, int size);
+int printhexaupper(va_list type, char buffer[],
+	int flag, int width, int precision, int size);
+
+int printhexa(va_list type, char mapto[],
+char buffer[], int flag, char flagch, int width, int precision, int size);
+int printnonprintable(va_list type, char buffer[],
+	int flag, int width, int precision, int size);
+
+int printpointer(va_list type, char buffer[],
+	int flag, int width, int precision, int size);
+
+int flag(const char *format, int *i);
+int width(const char *format, int *i, va_list list);
+int precision(const char *format, int *i, va_list list);
+int size(const char *format, int *i);
+
+int printreverse(va_list type, char buffer[],
+	int flag, int width, int precision, int size);
+
+int printrot13string(va_list type, char buffer[],
+	int flag, int width, int precision, int size);
+
+int writechar(char c, char buffer[],
+	int flag, int width, int precision, int size);
+int writenumber(int ispositive, int ind, char buffer[],
+	int flag, int width, int precision, int size);
+int writenum(int ind, char bff[], int flags, int width, int precision,
+	int length, char pat, char extrac);
+int writepointer(char buffer[], int ind, int length,
+	int width, int flag, char pat, char extrac, int patstart);
+
+int writeunsgnd(int isnegative, int ind,
+char buffer[],
+	int flag, int width, int precision, int size);
+
+int isprintable(char);
+int appendhexacode(char, char[], int);
+int isdigit(char);
+
+long int convertsizenumber(long int num, int size);
+long int convertsizeunsgnd(unsigned long int num, int size);
 
 #endif
---
+++ write.c
@@ -0,0 +1,244 @@
+#include "main.h"
+/**
+ * writechar - Prints a string
+ * @c: char types.
+ * @buffer: Buffer array
+ * @flag:  Calculates active flags.
+ * @width: get width.
+ * @precision: precision specifier
+ * @size: Size specifier
+ *
+ * Return: Number of chars printed.
+ */
+int writchar(char c, char buffer[],
+	int flag, int width, int precision, int size)
+{
+	int i = 0;
+	char pat = ' ';
+
+	UNUSED(precision);
+	UNUSED(size);
+
+	if (flag & T_ZERO)
+		pat = '0';
+
+	buffer[i++] = c;
+	buffer[i] = '\0';
+
+	if (width > 1)
+	{
+		buffer[BUFSIZE - 1] = '\0';
+		for (i = 0; i < width - 1; i++)
+			buffer[BUFSIZE - i - 2] = pat;
+
+		if (flag & T_MINUS)
+			return (write(1, &buffer[0], 1) +
+					write(1, &buffer[BUFSIZE - i - 1], width - 1));
+		else
+			return (write(1, &buffer[BUFSIZE - i - 1], width - 1) +
+					write(1, &buffer[0], 1));
+	}
+
+	return (write(1, &buffer[0], 1));
+}
+
+/**
+ * writenumber - Prints a string
+ * @isnegative: Lista of arguments
+ * @ind: char types.
+ * @buffer: Buffer array
+ * @flag:  Calculates active flags
+ * @width: get width.
+ * @precision: precision specifier
+ * @size: Size specifier
+ * Return: Number of chars printed.
+ */
+int writenumber(int isnegative, int ind, char buffer[],
+	int flag, int width, int precision, int size)
+{
+	int length = BUFSIZE - ind - 1;
+	char pat = ' ', extrach = 0;
+
+	UNUSED(size);
+
+	if ((flag & T_ZERO) && !(flag & T_MINUS))
+		pat = '0';
+	if (isnegative)
+		extrach = '-';
+	else if (flag & T_PLUS)
+		extrach = '+';
+	else if (flag & T_SPACE)
+		extrach = ' ';
+
+	return (writenum(ind, buffer, flag, width, precision,
+		length, pat, extrach));
+}
+
+/**
+ * writenum - Write a number using a bufffer
+ * @ind: Index at which the number starts on the buffer
+ * @buffer: Buffer
+ * @flag: Flags
+ * @width: width
+ * @precision: Precision specifier
+ * @length: Number length
+ * @pat: Pading char
+ * @extrac: Extra char
+ * Return: Number of printed chars.
+ */
+int writenum(int ind, char buffer[],
+	int flag, int width, int precision,
+	int length, char pat, char extrac)
+{
+	int i, patstart = 1;
+
+	if (precision == 0 && ind == BUFSIZE - 2 && buffer[ind] == '0' && width == 0)
+		return (0);
+	if (precision == 0 && ind == BUFSIZE - 2 && buffer[ind] == '0')
+		buffer[ind] = pat = ' '; 
+	if (precision > 0 && precision < length)
+		pat = ' ';
+	while (precision > length)
+		buffer[--ind] = '0', length++;
+	if (extrac != 0)
+		length++;
+	if (width > length)
+	{
+		for (i = 1; i < width - length + 1; i++)
+			buffer[i] = pat;
+		buffer[i] = '\0';
+		if (flag & T_MINUS && pat == ' ')
+		{
+			if (extrac)
+				buffer[--ind] = extrac;
+			return (write(1, &buffer[ind], length) + write(1, &buffer[1], i - 1));
+		}
+		else if (!(flag & T_MINUS) && pat == ' ')
+		{
+			if (extrac)
+				buffer[--ind] = extrac;
+			return (write(1, &buffer[1], i - 1) + write(1, &buffer[ind], length));
+		}
+		else if (!(flag & T_MINUS) && pat == '0')
+		{
+			if (extrac)
+				buffer[--patstart] = extrac;
+			return (write(1, &buffer[patstart], i - patstart) +
+				write(1, &buffer[ind], length - (1 - patstart)));
+		}
+	}
+	if (extrac)
+		buffer[--ind] = extrac;
+	return (write(1, &buffer[ind], length));
+}
+
+/**
+ * writeunsgnd - Writes an unsigned number
+ * @isnegative: Number indicating if the num is negative
+ * @ind: Index at which the number starts in the buffer
+ * @buffer: Array of chars
+ * @flag: Flags specifiers
+ * @width: Width specifier
+ * @precision: Precision specifier
+ * @size: Size specifier
+ * Return: Number of written chars.
+ */
+int writeunsgnd(int isnegative, int ind,
+	char buffer[],
+	int flag, int width, int precision, int size)
+{
+	int length = BUFSIZE - ind - 1, i = 0;
+	char pat = ' ';
+
+	UNUSED(isnegative);
+	UNUSED(size);
+
+	if (precision == 0 && ind == BUFSIZE - 2 && buffer[ind] == '0')
+		return (0);
+
+	if (precision > 0 && precision < length)
+		pat = ' ';
+
+	while (precision > length)
+	{
+		buffer[--ind] = '0';
+		length++;
+	}
+
+	if ((flag & t_ZERO) && !(flag & T_MINUS))
+		pat = '0';
+
+	if (width > length)
+	{
+		for (i = 0; i < width - length; i++)
+			buffer[i] = padd;
+
+		buffer[i] = '\0';
+
+		if (flag & T_MINUS)
+		{
+			return (write(1, &buffer[ind], length) + write(1, &buffer[0], i));
+		}
+		else
+		{
+			return (write(1, &buffer[0], i) + write(1, &buffer[ind], length));
+		}
+	}
+
+	return (write(1, &buffer[ind], length));
+}
+
+/**
+ * writepointer - Write a memory address
+ * @buffer: Arrays of chars
+ * @ind: Index at which the number starts in the buffer
+ * @length: Length of number
+ * @width: Width specifier
+ * @flag: Flags specifier
+ * @pat: Char representing the padding
+ * @extrac: Char representing extra char
+ * @patstart: Index at which padding should start
+ * Return: Number of written chars.
+ */
+int writepointer(char buffer[], int ind, int length,
+	int width, int flag, char pat, char extrac, int patstart)
+{
+	int i;
+
+	if (width > length)
+	{
+		for (i = 3; i < width - length + 3; i++)
+			buffer[i] = pat;
+		buffer[i] = '\0';
+		if (flag & T_MINUS && pat == ' ')
+		{
+			buffer[--ind] = 'x';
+			buffer[--ind] = '0';
+			if (extrac)
+				buffer[--ind] = extrac;
+			return (write(1, &buffer[ind], length) + write(1, &buffer[3], i - 3));
+		}
+		else if (!(flag & T_MINUS) && pat == ' ')
+		{
+			buffer[--ind] = 'x';
+			buffer[--ind] = '0';
+			if (extrac)
+				buffer[--ind] = extrac;
+			return (write(1, &buffer[3], i - 3) + write(1, &buffer[ind], length));
+		}
+		else if (!(flag & T_MINUS) && pat == '0')
+		{
+			if (extrac)
+				buffer[--patstart] = extrac;
+			buffer[1] = '0';
+			buffer[2] = 'x';
+			return (write(1, &buffer[patstart], i - patstart) +
+				write(1, &buffer[ind], length - (1 - patstart) - 2));
+		}
+	}
+	buffer[--ind] = 'x';
+	buffer[--ind] = '0';
+	if (extrac)
+		buffer[--ind] = extrac;
+	return (write(1, &buffer[ind], BUFSIZE - ind - 1));
+}
